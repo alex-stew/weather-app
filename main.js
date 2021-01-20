b3e261b31cb2fa3086031a01ccce103167bd77f6
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-01-20 15:44:15 +1030

    changes to js, html and css files to recieve data through the api (not yet displaying) and see design aspects

--- main.js
+++ main.js
@@ -8,8 +8,20 @@ searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
     if (evt.keyCode == 13) {
-        // getResults(searchbox.value);
+        getResults(searchbox.value);
         console.log(searchbox.value);
     }
 }
 
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = ``
+}
